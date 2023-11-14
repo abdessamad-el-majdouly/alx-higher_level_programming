49b4a1616991d5b6fdb4614e577ef66dbcfe5d95
Abdessamad El Majdouly <33199278+abdessamad-el-majdouly@users.noreply.github.com>
2023-11-15 00:29:59 +0100

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
-
-// JS script to define a class, creates an instance method to print rectangle
 class Rectangle {
   constructor(w, h) {
-    if (w === undefined || h === undefined || w <= 0 || h <= 0) {
-      return; // create an empty object
-    } else {
-      this.width = w;
-      this.height = h;
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      throw new Error('Invalid rectangle dimensions');
     }
+
+    this.width = w;
+    this.height = h;
   }
 
   print() {
-    if (this.width && this.height) {
-      for (let count = 0; count < this.height; count++) {
-        console.log('X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
       }
+      console.log('');
     }
   }
 }
