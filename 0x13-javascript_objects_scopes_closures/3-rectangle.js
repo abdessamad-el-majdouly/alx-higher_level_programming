268971a2d6aaacb50b32a34ece423bd682a964a5
Abdessamad El Majdouly <33199278+abdessamad-el-majdouly@users.noreply.github.com>
2023-11-15 00:35:23 +0100

    Update 3-rectangle.jsx

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
-// JS script to define a class, creates an instance method to print rectangle
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      throw new Error('Invalid rectangle dimensions');
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
-
-    this.width = w;
-    this.height = h;
   }
 
-  print() {
-    for (let count = 0; count < this.height; count++) {
-      console.log('X'.repeat(this.width));
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
     }
   }
 }
+
 module.exports = Rectangle;
